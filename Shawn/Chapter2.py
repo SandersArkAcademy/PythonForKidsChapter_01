fad45f63782e4b151aa75c0314ba167991f170bd
J. Shawn Sanders <j.shawn.sanders@gmail.com>
2021-02-20 18:51:49 -0600

    This to reshow how to commit.

---
+++ Shawn/Chapter2.py
@@ -0,0 +1,16 @@
+#%%
+8 * 3.57
+
+#%%
+number_of_coins = 200
+print(number_of_coins)
+
+#%%
+
+found_coins = 20
+magic_coins = 100
+magic_coins = 10
+stolen_coins = 3
+
+found_coins + magic_coins * 365 - stolen_coins *52
+# %%
--- Shawn/HelloWorld.py
+++ Shawn/HelloWorld.py
@@ -1,1 +1,8 @@
-print('Hello World')
+#%%
+
+helloWord = "Here is something you don't see a lot of."
+howManyTimes = 9000
+print(helloWord *howManyTimes)
+#%%
+# %%
+# This will not be run.
