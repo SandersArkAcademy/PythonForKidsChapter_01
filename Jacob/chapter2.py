7349f0456a6d0f81d4539338e825458605c2a9d8
TxJake <78991879+TxJake@users.noreply.github.com>
2021-02-25 18:11:30 -0600

    Chapter 2 complete

---
+++ Jacob/chapter2.py
@@ -0,0 +1,48 @@
+#%%
+8 * 3.57
+#%%
+10 * 365
+20 + 3650
+#%%
+3 * 52
+3670 - 156
+#%%
+5 + 30 * 20
+#%%
+(5 + 30) * 20
+#%%
+((5 + 30) * 20) / 10
+#%%
+5 + 30 * 20 / 10
+#%%
+fred = 100
+#%%
+print(fred)
+#%%
+fred = 200
+print(fred)
+#%%
+fred = 200
+john = fred
+print(john)
+#%%
+number_of_coins = 200
+print(number_of_coins)
+#%%
+20 + 10 * 365
+3 * 52
+3670 - 156
+#%%
+20 + 10 * 365 - 3 * 52
+#%%
+found_coins = 20
+magic_coins = 10
+stolen_coins = 3
+#%%
+found_coins + magic_coins * 365 - stolen_coins * 52
+#%%
+stolen_coins = 2
+#%%
+magic_coins = 13
+found_coins + magic_coins * 365 - stolen_coins * 52
+#%%
