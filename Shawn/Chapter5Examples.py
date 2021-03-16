45bb1d40b4604e38cf44fb18432dc360d996f893
J. Shawn Sanders <j.shawn.sanders@gmail.com>
2021-03-15 21:30:38 -0500

    Chapter 5 example answers for reference.

---
+++ Shawn/Chapter5Examples.py
@@ -0,0 +1,33 @@
+#%%
+#Example 1
+# Answer it will output "I am rich"
+
+#Example 2
+twinkies = 15
+
+if twinkies < 100 or twinkies > 500:
+    print("Too few or too many.")
+#%%
+#Example 3
+
+money = 15
+if money > 100 and money < 500:
+    print("It is between $100 and $500.")
+elif money >1000 and money < 5000:
+    print("It is between $1000 and $5000.")
+else:
+    print("Not even close.")
+
+
+# %%
+#Example 4
+ninjas = 31
+
+if ninjas < 10:
+    print("I can fight those ninjas!")
+elif ninjas < 30:
+    print("It\'ll be a struggle, but I can take \'em")
+elif ninjas < 50:
+    print("That\'s too many")
+
+# %%
