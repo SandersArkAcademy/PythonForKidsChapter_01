fd5196347024abbef88aba2f965d0db0cb03e2fa
J. Shawn Sanders <j.shawn.sanders@gmail.com>
2021-03-15 21:08:12 -0500

    Added cells so we could run different cells.

--- Inara/Chapter5.py
+++ Inara/Chapter5.py
@@ -1,39 +1,40 @@
+#%%
 age = 13
 if age > 20:
     print('You are too old!')
-
+#%%
 age = 25
 if age > 20:
     print('You are too old!')
     print('Why are you here?')
     print('Why aren\'t you mowing a lawn or sorting papers?')
-
+#%%
 age = 10
 if age > 10:
     print('You are too old for my jokes!')
-
+#%%
 age = 10
 if age >= 10:
     print('You are too old for my jokes!')
-
+#%%
 age = 10
 if age == 10:
     print('What\'s brown and sticky? A stick!')
-
+#%%
 print("Want to hear a dirty joke?")
 age = 12
 if age == 12:
     print("A pig fell in the mud!")
 else:
     print("Shh. It\'s a secret.")
-
+#%%
 print("Want to hear a dirty joke?")
 age = 8
 if age == 12:
     print("A pig fell in the mud!")
 else:
     print("Shh. It\'s a secret.")
-
+#%%
 age = 12
 if age == 10:
     print("What do you call an unhappy cranberry?")
@@ -49,52 +50,52 @@ elif age == 13:
     print("Because rather than eating 9, 7 8 pi.")
 else:
     print("Huh?")
-
+#%%
 if age == 10 or age == 11 or age == 12 or age == 13:
     print('What is 13 + 49 + 84 + 155 + 97? A headache!')
 else:
     print('Huh?')
-
+#%%
 if age >= 10 and age <= 13:
     print('What is 13 + 49 + 84 + 155 + 97? A headache!')
 else:
     print('Huh?')
-
+#%%
 myval = None
 print(myval)
-
+#%%
 myval = None
 if myval == None:
     print("The variable myval doesn\'t have a value")
-
+#%%
 age = 10
 if age == 10:
     print("What\'s the best way to speak to a monster?")
     print("From as far away as possible!")
-
+#%%
 age = '10'
 if age == 10:
     print("What\'s the best way to speak to a monster?")
     print("From as far away as possible!")
-
+#%%
 age = '10'
 converted_age = int(age)
-
+#%%
 age = 10
 converted_age = str(age)
-
+#%%
 age = '10'
 converted_age = int(age)
 if converted_age == 10:
     print("What\'s the best way to speak to a monster?")
     print("From as far away as possible!")
-
+#%%
 age = '10.5'
 converted_age = int(age)
-
+#%%
 age = '10.5'
 converted_age = float(age)
 print(converted_age)
-
+#%%
 age = 'ten'
 converted_age = int(age)
