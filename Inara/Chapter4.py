8ec58adc6a1548fdff15c2708d70fc86808fe38b
AmongUsrules <78991452+AmongUsrules@users.noreply.github.com>
2021-03-26 18:35:29 -0500

    Fox! We can't go here the map says-

--- Inara/Chapter4.py
+++ Inara/Chapter4.py
@@ -20,6 +20,7 @@ t.right(90)
 
 
 val = input("Hit enter for next example")
+
 t.clear()
 t.reset()
 
@@ -30,4 +31,45 @@ for i in range(4):
     t.forward(50)
     t.right(90)
 
-val = input
+
+val = input("Hit enter for next example.")
+
+t.clear()
+t.setpos(0,0)
+print("Make a star.")
+
+
+for i in range(5):
+    t.forward(50)
+    t.right(144)
+
+val = input("Hit enter for next example.")
+
+t.clear()
+t.setpos(0,0)
+print("Make a star 2.")
+
+for i in range(78):
+    t.forward(i * 10)
+    t.right(144)
+
+val = input("Hit enter for next example.")
+
+t.clear()
+t.setpos(0,0)
+print("Make a star 2.")
+
+t.clear()
+t.setpos(0,0)
+t.pencolor("blue")
+
+for i in range(50):
+    t.forward(50)
+    t.left(123)
+
+t.pencolor("red")
+for i in range(50):
+    t.forward(100)
+    t.left(123)
+
+turtle.done()
