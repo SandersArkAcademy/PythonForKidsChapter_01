6c71d979d87074565fcfa66d5be599f5e55802cf
AmongUsrules <78991452+AmongUsrules@users.noreply.github.com>
2021-03-26 19:50:24 -0500

    I'M MAKING MAC AND CHEESE AND NO ONE'S GONNA STOP-

---
+++ Inara/Chapter5examples.py
@@ -0,0 +1,30 @@
+ money = 2000
+if money > 1000:
+    print("I'm rich! !")
+else:
+    print("I'm not rich! !")
+    print("But I might be later...")
+
+    val = input("Hit enter for next example")
+
+Twinkies = 374
+if Twinkies < 100 or Twinkies > 500:
+    print("Too few or too many")
+
+val = input("Hit enter for next example")
+
+Money = 4949
+if Money > 100 and Money < 500 or Money > 1000 and Money < 5000:
+     print("I can buy SO many video games!!!!!")
+else:
+     print("Man I really wanted Party Panic but I can't afford it!")
+
+val = input("Hit enter for next example")
+ninjas = 86
+
+if ninjas < 10:
+    print("I can fight those ninjas!")
+elif ninjas < 30:
+    print("It\'ll be a struggle, but I can take \'em")
+elif ninjas < 50:
+    print("That\'s too many")
