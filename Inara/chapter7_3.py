3ea08f031eb4d38cbd0bcc9aed2f681a57c1f05a
AmongUsrules <78991452+AmongUsrules@users.noreply.github.com>
2022-07-14 09:21:48 -0500

    Puzzle 2 complete

--- Inara/chapter7_3.py
+++ Inara/chapter7_3.py
@@ -1,12 +1,12 @@
-#Programming puzzles 1
+#Programming puzzles 2
 
-def moon_weight(OGearth_weight, OGearth_weightIncrease):
-   # OGearth_weight = 38.6
-    moon_Converstion_Factor = 0.165
-    for year in range(0,15):
+def moon_weight(OGearth_weight, OGearth_weightIncrease,num_Years_to_Run):
+   
+    MOON_CONVERSION_FACTOR = 0.165
+    for year in range(0,num_Years_to_Run):
         earth_weight = OGearth_weight + (year * OGearth_weightIncrease)
-        moon_weight = earth_weight * moon_Converstion_Factor
+        moon_weight = earth_weight * MOON_CONVERSION_FACTOR
         print('Year:%s Earth weight: %sKg Moon weight:%sKg' %(year +1,earth_weight,moon_weight))
 
 
-moon_weight(38.6,1)
+moon_weight(38.6,1,15)
